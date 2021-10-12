73591d7bb202dea0c5b5088363dddce79473c57d
viktorsht <viktorshantus@gmail.com>
2021-10-12 10:17:07 -0300

    feito

---
+++ speedtest.py
@@ -0,0 +1,14 @@
+import speedtest
+
+test = speedtest.Speedtest()
+
+down = test.download()
+rsDown = round(down)
+fDown = int(rsDown /1e+6)
+
+upload = test.upload()
+rsUp = round(upload)
+fUp = int(rsUp /1e+6)
+
+print(f"Download: {fDown} mb/s")
+print(f"Upload: {fUp} mb/s")
